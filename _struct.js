d1928569d734f50d6a75b143f39015498885b5fc
Kyle Traff <ktraff@gmail.com>
2013-07-09 23:50:27 -0600

    initial commit

---
+++ _struct.js
@@ -0,0 +1,14 @@
+//     _struct.js 0.0.1
+//     http://structjs.org
+//     (c) 2013 Kyle Traff
+//     _struct may be freely distributed under the MIT license.
+
+(function() {
+
+  // Baseline setup
+  // --------------
+
+  // Establish the root object, `window` in the browser, or `global` on the server.
+  var root = this;
+
+}).call(this);
