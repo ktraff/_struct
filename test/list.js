55f0c183d9fba6aa216c0673cb4d404ec58d8496
Kyle Traff <ktraff@gmail.com>
2013-07-15 08:02:03 -0600

    small code edit

--- test/list.js
+++ test/list.js
@@ -16,11 +16,11 @@ $(function () {
     var list = _.list([1, 2, 3]);
     var elem = list.find(1);
     equal(elem, 1, 'could not find the right element in the list');
-    var elem = list.find(3);
+    elem = list.find(3);
     equal(elem, 3, 'could not find the right element in the list');
-    var elem = list.find(2);
+    elem = list.find(2);
     equal(elem, 2, 'could not find the right element in the list');
-    var elem = list.find(4);
+    elem = list.find(4);
     strictEqual(elem, undefined, 'expected not to find 4, but found something other than `undefined`');
   });
 });
