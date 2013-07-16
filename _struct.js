6180a85f52693540c2cfc9b74a88a01e74a4b014
Kyle Traff <ktraff@gmail.com>
2013-07-16 13:57:10 -0600

    object chaining for list functions, with new object creation where destructive updates were needed.  bug fix to exports() to allow multiple list creation

--- _struct.js
+++ _struct.js
@@ -31,7 +31,7 @@
     options = _.extend({
       // Define a filter function to remove unwanted or private struct attributes.
       filter: _.bind(function (prop) {
-        return !this.hasOwnProperty(prop) || prop[0] === '_';
+        return !this.hasOwnProperty(prop);
       }, this)
     }, options);
     var struct = {};
@@ -42,6 +42,12 @@
     return struct;
   };
 
+  // An internal function that returns true if the given object 
+  // represents an object, but not a function or array
+  var isStrictObject = function (obj) {
+    return _.isObject(obj) && !_.isFunction(obj) && !_.isArray(obj);
+  };
+
   var VERSION = '0.0.1';
 
   // Underscore List
@@ -49,19 +55,33 @@
   var list = {
     VERSION: VERSION,
 
-    // Creates a new list object.
-    initialize: function (arr) {
-      if (_.isArray(arr)) {
-        this.struct = _.reduceRight(arr, this.cons, null, this);
+    // Creates a new list object. Pass an array of elements, a valid list struct 
+    // object, or leave empty to initialize an empty list.
+    initialize: function (obj) {
+      if (_.isArray(obj)) {
+        this.struct = _.reduceRight(obj, this._cons, null, this);
+      } else if (isStrictObject(obj)) {
+        this.struct = obj;
       } else {
-        this.struct = this.cons(null, {});
+        this.struct = null;
       }
       return this;
     },
 
     // Constructs a list element by placing the obj at the head of the list.
-    cons: function (list, obj) {
+    _cons: function (list, obj) {
       return { first: obj, rest: list };
+    },
+
+    // Exports a list instance for use in an **Underscore.js** mixin.
+    _exports: function () {
+      return {
+        list: _.bind(function (obj) {
+          var listObj = exports.call(this);
+          listObj.initialize(obj);
+          return listObj;
+        }, this)
+      };
     },
 
     // A modified version of _.each made for traversing lists.
@@ -74,42 +94,49 @@
       }
     },
 
-    // Exports a list instance for use in an **Underscore.js** mixin.
-    exports: function () {
-      var listObj = exports.call(this);
-      return {
-        list: _.bind(listObj.initialize, listObj)
-      };
-    },
-
-    // Retrieves an element from the list, if it exists.  
-    find: function (obj, iterator, context) {
+    // Retrieves an element from the list, if it exists. Return 
+    // a new list with the found element, or an empty list if not found.
+    find: function (token, iterator, context) {
       var result;
       var list = this.struct;
-      // If no iterator is given, simply check equality with the given obj
-      iterator = iterator || function (val, idx, list) {
-        return obj == val;
+      // If no iterator is given, simply check equality with the given token.
+      iterator = iterator || function (token, obj, list) {
+        return token == obj;
       };
       this.each(function (obj, idx, list) {
-        if (iterator.call(context, obj, idx, list)) {
-          result = obj;
+        if (iterator.call(context, token, obj, list)) {
+          result = list;
           return breaker;
         }
       }, context);
-      return result;
+      return _.list(result);
     },
 
+    // Creates a new list, with the given obj inserted at the head of the list.
+    insert: function (obj) {
+      var struct = this._cons(this.struct, obj);
+      return _.list(struct);
+    },
+
+    // Returns true if the current element is empty, and
+    // there exist no pointers to other elements in the list.
     isEmpty: function (arr) {
       return this.struct === null;
+    },
+
+    // Returns the current element in the list.
+    val: function () {
+      return this.struct ? this.struct.first : this.struct;
     }
   };
 
   var structs = {
     list: list
   };
+
   // Add all struct objects to the Underscore library
   _.each(structs, function (struct, key) {
-    _.mixin(struct.exports());
+    _.mixin(struct._exports());
     nodeExports(key, struct);
   });
 
--- test/list.js
+++ test/list.js
@@ -4,23 +4,52 @@ $(function () {
 
   test('Create a list', function() {
     var list = _.list([1, 2, 3]);
-    equal(typeof list, 'object', 'list is not an object');
+    equal(typeof list, 'object', 'list contains an initialized object');
   });
 
-  test('Create an empty list', function() {
+  test('Create an empty list with an empty array', function() {
     var list = _.list([]);
-    strictEqual(list.isEmpty(), true, 'list is not empty');
+    strictEqual(list.isEmpty(), true, 'list is initialized to an empty list');
+  });
+
+  test('Create an empty list with an empty constructor', function() {
+    var list = _.list();
+    strictEqual(list.isEmpty(), true, 'list is initialized to an empty list');
+  });
+
+  test('Create a list with an existing list', function() {
+    var list = _.list([1, 2, 3]);
+    var newList = _.list(list.struct);
+    var elem = newList.find(1);
+    equal(elem.val(), 1, 'new list contains elements from the original list');
   });
 
   test('Find an element in a list', function() {
     var list = _.list([1, 2, 3]);
-    var elem = list.find(1);
-    equal(elem, 1, 'could not find the right element in the list');
-    elem = list.find(3);
-    equal(elem, 3, 'could not find the right element in the list');
-    elem = list.find(2);
-    equal(elem, 2, 'could not find the right element in the list');
-    elem = list.find(4);
-    strictEqual(elem, undefined, 'expected not to find 4, but found something other than `undefined`');
+    var found = list.find(1);
+    equal(found.val(), 1, '`1` was found in the list');
+    found = list.find(3);
+    equal(found.val(), 3, '`3` was found in the list');
+    found = list.find(2);
+    equal(found.val(), 2, '`2` was found in the list');
+    found = list.find(4);
+    strictEqual(found.val(), null, '`4` was not found in the list');
+    found = list.find({ elem: 'value' });
+    strictEqual(found.val(), null, '`{ elem: \'value\' }` was not found in the list');
   });
+
+  test('Insert an element into a list', function() {
+    var obj = { key: 'value' };
+    var finder = function (token, obj, list) {
+      return token['key'] == obj['key'];
+    };
+    var list = _.list();
+    newList = list.insert(obj);
+    var found = list.find(obj, finder);
+    equal(found.val(), null, '`obj` was not found in the original list');
+    var found = newList.find(obj);
+    equal(found.val(), obj, '`obj` was found in the new list');
+  });
+
+
 });
